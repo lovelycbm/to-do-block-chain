225554d98fcf37edbd17e43b6773d21ec19dfaf5
lovelycbm <lovelycbm@naver.com>
2021-05-04 17:29:22 +0900

    3. create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint256 public taskCount;
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    event TaskCreated(uint256 id, string content, bool completed);
+
+    constructor() public {
+        createTask("Check out!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
 }
